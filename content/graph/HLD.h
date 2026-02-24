c519f42c39552cd9cc2b5f3032bd85d5a4dd84e7
duduFreire <dudufreiresantos@gmail.com>
2026-02-24 18:28:52 -0300

    generic HLD

--- content/graph/HLD.h
+++ content/graph/HLD.h
@@ -1,65 +1,66 @@
 /**
- * Author: Benjamin Qi, Oleksandr Kulkov, chilli
- * Date: 2020-01-12
+ * Author: duduFreire, Benjamin Qi, Oleksandr Kulkov, chilli
+ * Date: 2026-24-02
  * License: CC0
  * Source: https://codeforces.com/blog/entry/53170, https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/HLD%20(10.3).h
- * Description: Decomposes a tree into vertex disjoint heavy paths and light
- * edges such that the path from any leaf to the root contains at most log(n)
- * light edges. Code does additive modifications and max queries, but can
- * support commutative segtree modifications/queries on paths and subtrees.
- * Takes as input the full adjacency list. VALS\_EDGES being true means that
- * values are stored in the edges, as opposed to the nodes. All values
- * initialized to the segtree default. Root must be 0.
- * Time: O((\log N)^2)
- * Status: stress-tested against old HLD
+ * Description: Does the same thing as a LazySegment tree, but with an extra log factor to the complexity. VAL_EDGES
+ * determines if the values are stored in edges. If so, store the value of each edge on its child verted.
+ * Time: O((\log N)^2) for range operations, O(log(n)) for point operations
+ * Status: AC on Police Hypothesis
  */
 #pragma once
 
 #include "../data-structures/LazySegmentTree.h"
 
-template <bool VALS_EDGES> struct HLD {
-	int N, tim = 0;
-	vector<vi> adj;
-	vi par, siz, rt, pos;
-	Node *tree;
-	HLD(vector<vi> adj_)
-		: N(sz(adj_)), adj(adj_), par(N, -1), siz(N, 1),
-		  rt(N),pos(N),tree(new Node(0, N)){ dfsSz(0); dfsHld(0); }
-	void dfsSz(int v) {
-		for (int& u : adj[v]) {
-			adj[u].erase(find(all(adj[u]), v));
-			par[u] = v;
-			dfsSz(u);
+template<class S, bool VAL_EDGES>
+struct HLD {
+	using T = typename S::T; using L = typename S::L;
+	int n,tim=0;
+	vector<vi> g;
+	vi p,siz,hd,ti;
+	SegBeats<S> seg, segi;
+	HLD(vector<vi> ag) : n(sz(ag)), g(ag), p(n), siz(n), hd(n), ti(n),
+	seg(bit_ceil((unsigned)n)), segi(bit_ceil((unsigned)n)) { dfs(0); dfs2(0);}
+	void dfs(int v) {
+		for (int& u : g[v]) {
+			g[u].erase(find(all(g[u]), v));
+			p[u]=v;
+			dfs(u);
 			siz[v] += siz[u];
-			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
+			if (siz[u] > siz[g[v][0]]) swap(u, g[v][0]);
 		}
 	}
-	void dfsHld(int v) {
-		pos[v] = tim++;
-		for (int u : adj[v]) {
-			rt[u] = (u == adj[v][0] ? rt[v] : u);
-			dfsHld(u);
+	void dfs2(int v) {
+		ti[v]=tim++;
+		for (int u : g[v]) {
+			hd[u] = (u == g[v][0] ? hd[v] : u);
+			dfs2(u);
 		}
 	}
-	template <class B> void process(int u, int v, B op) {
-		for (;; v = par[rt[v]]) {
-			if (pos[u] > pos[v]) swap(u, v);
-			if (rt[u] == rt[v]) break;
-			op(pos[rt[v]], pos[v] + 1);
+	template <class B, class C> // ops are [l, r)
+	void process(int u, int v, B op1, C op2) {
+		for (;;) {
+			if (hd[u] == hd[v]) break;
+			if (ti[u] < ti[v]) op2(ti[hd[v]], ti[v]+1), v=p[hd[v]];	
+			else op1(ti[hd[u]], ti[u]+1), u=p[hd[u]];
 		}
-		op(pos[u] + VALS_EDGES, pos[v] + 1);
+		if (ti[u] < ti[v]) op2(ti[u]+VAL_EDGES, ti[v]+1);
+		else op1(ti[v]+VAL_EDGES, ti[u]+1);
 	}
-	void modifyPath(int u, int v, int val) {
-		process(u, v, [&](int l, int r) { tree->add(l, r, val); });
+	T query(int u, int v) { // inclusive on both ends
+		T vu = S::id, vv = S::id;
+		process(u,v,
+		  [&](int l, int r){ vu = S::op(vu, segi.query(n-r,n-l)); },
+		  [&](int l, int r){ vv = S::op(seg.query(l,r), vv); } );	
+		return S::op(vu,vv);
 	}
-	int queryPath(int u, int v) { // Modify depending on problem
-		int res = -1e9;
-		process(u, v, [&](int l, int r) {
-				res = max(res, tree->query(l, r));
-		});
-		return res;
+	void update(L val, int u, int v) {
+		process(u,v,
+		  [&](int l, int r){ segi.update(val,n-r,n-l); },
+		  [&](int l, int r){ seg.update(val,l,r);  });	
 	}
-	int querySubtree(int v) { // modifySubtree is similar
-		return tree->query(pos[v] + VALS_EDGES, pos[v] + siz[v]);
+	void update(L val, int u) {
+		seg.update(val,ti[u], ti[u]+1);
+		segi.update(val,n-ti[u]-1, n-ti[u]);
 	}
 };
