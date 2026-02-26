c06d3c2fc7017d1911a2243d8bff3d7f4878b5a5
Arthur Botelho <arthur.botelho@incognia.com>
2026-02-26 14:44:16 -0300

    small optimizations

--- content/math/PolyRoots.h
+++ content/math/PolyRoots.h
@@ -8,8 +8,23 @@
  */
 #pragma once
 
-#include "Polynomial.h"
-
+struct Poly {
+	vector<double> a;
+	double operator()(double x) const {
+		double val = 0;
+		for (int i = sz(a); i--;) (val *= x) += a[i];
+		return val;
+	}
+	void diff() {
+		rep(i,1,sz(a)) a[i-1] = i*a[i];
+		a.pop_back();
+	}
+	void divroot(double x0) {
+		double b = a.back(), c; a.back() = 0;
+		for(int i=sz(a)-1; i--;) c = a[i], a[i] = a[i+1]*x0+b, b=c;
+		a.pop_back();
+	}
+};
 vector<double> polyRoots(Poly p, double xmin, double xmax) {
 	if (sz(p.a) == 2) { return {-p.a[0]/p.a[1]}; }
 	vector<double> ret;
